d160a54184e12ddea2c6ec637199dac4fa2f6620
Aaron-石雪岩 <Aaron3963@163.com>
2022-09-23 10:20:27 -0700

    Update for MultiClass Enroll

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-from ast import While
-from time import sleep
+from ast import Try, While
+from time import sleep, time
+from xmlrpc.client import Boolean
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select,WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 
 import openpyxl
 bk=openpyxl.load_workbook('config.xlsx')
@@ -16,16 +17,20 @@ maxCol=sheet.max_column
 USERNAME = sheet.cell(1,2).value
 PASSWORD = sheet.cell(2,2).value
 
-dict={}
+dict = {}
+courseChecklist = {}
 for i in range(3,maxRow+1):
     list = []
     for j in range(2,maxCol+1):
         cell = str(sheet.cell(i,j).value)
+        if cell == 'None':
+            continue
         if len(cell) < 6:
             for numZero in range (0,6-len(cell)):
                 cell = '0'+cell
         list.append(cell)
     dict[sheet.cell(i,1).value] = list
+    courseChecklist [sheet.cell(i,1).value] = False
 print(dict)
 
 
@@ -44,13 +49,15 @@ wd.find_element(By.XPATH,'//*[@id="login"]/button').click()
 sleep(15)
 
 #Go page
-termSelect = Select(wd.find_element(By.ID,'startpage-select-term'))
-termSelect.select_by_visible_text('Fall Quarter 2022')
+def goPage(wd):
+    wd.get('https://act.ucsd.edu/webreg2')
+    termSelect = Select(wd.find_element(By.ID,'startpage-select-term'))
+    termSelect.select_by_visible_text('Fall Quarter 2022')
 
-wd.find_element(By.ID,'startpage-button-go').click()
+    wd.find_element(By.ID,'startpage-button-go').click()
 
 
-def search(wd,course,id):
+def search(wd,course):
     searchBar = wd.find_element(By.ID, 's2id_autogen1')
     searchBar.clear()
     searchBar.send_keys(course)
@@ -64,12 +71,13 @@ def search(wd,course,id):
     #Expand Section List
     wd.find_element(By.XPATH, '//*[@id="search-div-b-tableghead_0_0"]/td/span').click()
     
+
+def check(wd,course,id):
     sectionEnrollID = 'search-enroll-id-'+str(id)
     sectionWaitID = 'search-wait-id-'+str(id)
 
     wd.implicitly_wait(0.05)
     try:
-        #targetClass = wd.find_element(By.ID,sectionEnrollID)
         targetClass = wd.find_element(By.ID,sectionEnrollID)
     except Exception:
         targetClass = wd.find_element(By.ID,sectionWaitID)
@@ -80,23 +88,86 @@ def search(wd,course,id):
         print("Class Mismatch. Abort.")
 
     if 'Waitlist' == targetClass.get_attribute('value'):
-        print(course+sectionID+": No Space, retrying...")
+        print(course+id+": No Space, retrying...")
         return False
 
     if 'Enroll' == targetClass.get_attribute('value'):
         #wd.find_element(By.ID)
         return True
 
+def enroll(wd,course,id,checklist):
+    sectionEnrollID = 'search-enroll-id-'+str(id)
 
-enrolled = False
-while True:
-    for course in dict.keys():
-        for sectionID in dict[course]:
-            enrolled = search(wd,course,sectionID)
-            if enrolled:
-                break
-        if enrolled:
-            break
-    if enrolled:
-        break
+    wd.find_element(By.ID,sectionEnrollID).click()
 
+    targetClass = wd.find_element(By.ID,'dialog-enroll').find_element(By.CLASS_NAME, 'dialog-tip-class')
+
+    enroll = False
+   
+    sleep(1) #Must use sleep for web to load new element
+    error = targetClass.find_elements(By.CLASS_NAME,'error')
+
+    # print(len(error))
+
+    if len(error) == 0:
+        if "Confirm" in targetClass.get_attribute('textContent'):
+            print("No Error Detected, Proceed Enrollment")
+            enroll = True
+
+    if enroll:
+        try:
+            wd.find_element(By.XPATH,'/html/body/div[28]/div[3]/div/button[2]').click()
+        except Exception:
+            wd.find_element(By.XPATH,'/html/body/div[28]/div[3]/div/button[2]').click()
+
+        try:
+            wd.find_element(By.ID,'dialog-after-action-email').click()
+        except Exception:
+            wd.find_element(By.ID,'dialog-after-action-email').click()
+            checklist[course] = True
+            print("Successfully enrolled in "+course+id+"!")
+            
+    elif not enroll:
+        try:
+            wd.find_element(By.ID,'dialog-after-action-close').click()
+        except Exception:
+            wd.find_element(By.ID,'dialog-after-action-close').click()
+
+
+    return enroll
+
+    
+def checkCourseList(checklist):
+    for enrolled in checklist.values():
+        if not enrolled:
+            return False
+    return True
+
+
+def main():
+    startTime = time()
+    passedTime = startTime
+    while passedTime - startTime < 30:
+        for course in dict.keys():
+            if courseChecklist[course]:
+                continue
+            search(wd,course)
+            for sectionID in dict[course]:
+                vacant = check(wd,course,sectionID)
+                if vacant:
+                    enrolled = enroll(wd,course,sectionID,courseChecklist)
+                    if enrolled:
+                        return True
+                passedTime = time()
+    return False
+
+goPage(wd)
+main()
+
+while not checkCourseList(courseChecklist):
+    print(courseChecklist)
+    print("Refresh Page")
+    goPage(wd)
+    main()
+
+wd.quit()
